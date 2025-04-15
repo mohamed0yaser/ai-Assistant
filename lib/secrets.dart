dcc71c3ec8800cdf91f32585e25146e3cbd7188e
mohamed yaser <90569015+mohamed0yaser@users.noreply.github.com>
2025-04-15 04:46:26 +0200

    Update secrets.dart

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,4 +1,4 @@
 class Secrets {
-static const  openAIAPIKey = 'AIzaSyDYKYbQc2IQ4CL5BjDKv2X5lwr_m3hwMD4';
-  static const openAIAPIURL = 'https://api.openai.com/v1/responses';
+static const  openAIAPIKey = '';
+  
 }
